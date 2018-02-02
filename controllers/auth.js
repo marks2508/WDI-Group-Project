2313690bf39d782e30d8b6decbdb0118a9a6f703
Mark S <markscicluna@hotmail.com>
2018-02-02 14:15:12 +0000

    auth added to controllers/auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+function register(req, res, next) {
+  User
+    .create(req.body)
+    .then(() => res.json({ message: 'Registration successful'}))
+    .catch(next);
+}
+
+function login(req, res, next) {
+  User
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if(!user || !user.validatePassword(req.body.password)) return res.unauthorized();
+
+      const token = jwt.sign({ userId: user.id }, { expiresIn: '1hr' });
+      return res.json({ token, message: `Welcome back ${user.username}` });
+    })
+    .catch(next);
+}
+
+module.exports = {
+  register,
+  login
+};
