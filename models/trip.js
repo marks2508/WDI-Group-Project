8167ad4f61d02e452ff0b36f5f7c494408b43ed8
MrBojangles <robbbert.thommmas@gmail.com>
2018-02-02 14:09:01 +0000

    removed createdBy for time being on trip model

--- models/trip.js
+++ models/trip.js
@@ -12,8 +12,8 @@ const tripSchema = new mongoose.Schema({
   startPointLat: { type: Number, required: true },
   startPointLng: { type: Number, required: true },
   endPointLat: { type: Number, required: true },
-  endPointLng: { type: Number, required: true },
-  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+  endPointLng: { type: Number, required: true }
+  // createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
   // comments: [ commentSchema ],
 });
 
