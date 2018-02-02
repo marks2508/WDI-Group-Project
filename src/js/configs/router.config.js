787918756a5e39767036a5415bfe446c031a8a17
Mark S <markscicluna@hotmail.com>
2018-02-02 15:16:53 +0000

    added index and register states in /js/config

--- src/js/configs/router.config.js
+++ src/js/configs/router.config.js
@@ -11,5 +11,16 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider) {
       url: '/trips',
       templateUrl: 'js/views/trips/index.html',
       controller: 'TripsIndexCtrl as vm'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'js/views/auth/login.html',
+      controller: 'LoginCtrl as vm'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'js/views/trips/register.html',
+      controller: 'RegisterCtrl as vm'
     });
+
 }
