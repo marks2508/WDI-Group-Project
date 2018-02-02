16cbff789fd7b8cc779ec3556157b2c350f7529a
MrBojangles <robbbert.thommmas@gmail.com>
2018-02-02 13:27:37 +0000

    cant get it working

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router  = express.Router();
+const posts = require('../controllers/posts');
+// const auth = require('../controllers/auth');
+// const users = require('../controllers/users');
+// const secureRoute = require('../lib/secureRoute');
 
 // Routes go here
+
+router.route('/posts')
+  .get(posts.index);
+
+router.route('/posts/:id')
+  .get(posts.show);
 
 module.exports = router;
