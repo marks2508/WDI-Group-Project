be484e78d86443158ad250e2e104bc448e680e3f
Mark S <markscicluna@hotmail.com>
2018-02-02 15:52:03 +0000

    added user controller

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+const User = require('../models/user');
+
+function showRoute(req, res, next) {
+  User
+    .findById(req.params.id)
+    .populate('trips')
+    .exec()
+    .then((user) => {
+      if(!user) return res.notFound();
+
+      res.json(user);
+    })
+    .catch(next);
+}
+
+module.exports = {
+  show: showRoute
+};
