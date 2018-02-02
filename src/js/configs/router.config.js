0e43c0668fcac1b18191754ac0069013acaf45c9
Jack Durham <jackdurham11@gmail.com>
2018-02-02 15:12:41 +0000

    added state

--- src/js/configs/router.config.js
+++ src/js/configs/router.config.js
@@ -7,9 +7,14 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 
   $stateProvider
-    .state('postsIndex', {
+    .state('tripsIndex', {
       url: '/trips',
       templateUrl: 'js/views/trips/index.html',
       controller: 'TripsIndexCtrl as vm'
+    })
+    .state('tripsNew', {
+      url: '/trips/new',
+      templateUrl: 'js/views/trips/new.html',
+      controller: 'TripsNewCtrl as vm'
     });
 }
