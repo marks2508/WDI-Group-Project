85ecd6386de6fe29127ca8497f612b90578177e2
Mark S <markscicluna@hotmail.com>
2018-02-02 13:53:09 +0000

    addded user model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.virtual('trips', {
+  ref: 'Trip',
+  localField: '_id', // use the _id field from this schema
+  foreignField: 'createdBy' // to match up with the createdBy field in the Post schema
+});
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.pre('validate', function checkPassword(next) {
+  if(!this._passwordConfirmation || this._passwordConfirmation !== this.password) {
+    this.invalidate('passwordConfirmation', 'does not match');
+  }
+  next();
+});
+
+userSchema.pre('save', function hashPassword(next) {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+userSchema.methods.validatePassword = function validatePassword(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', userSchema);
