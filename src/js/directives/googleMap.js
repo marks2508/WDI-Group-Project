6bab65bee32f2f5a61ec8f782fcaa22c965d5a23
MrBojangles <robbbert.thommmas@gmail.com>
2018-02-03 14:20:55 +0000

    google-maps stuff

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -8,10 +8,30 @@ function googleMap($window) {
     restrict: 'E',
     replace: true,
     template: '<div class="google-map">GOOGLE MAP GOES HERE</div>',
+    scope: {
+      center: '='
+    },
     link(scope, element) {
-      new $window.google.maps.Map(element[0], {
+      const map = new $window.google.maps.Map(element[0], {
         zoom: 4,
-        center: { lat: 51.5, lng: -0.07 }
+        center: scope.center
+      });
+      const infoWindow = new $window.google.maps.InfoWindow({
+        content: `${scope.center.lat} ${scope.center.lng}`
+      });
+      const marker = new $window.google.maps.Marker({
+        position: scope.center,
+        map
+      });
+      marker.addListener('click', () => {
+        infoWindow.open(map, marker);
+      });
+      map.addListener('click', (e) => {
+        console.log(e);
+        new $window.google.maps.Marker({
+          position: { lat: e.latLng.lat['[[scopes]]'][0].a, lng: e.latLng.lng['[[scopes]]'][0].a },
+          map
+        });
       });
     }
   };
