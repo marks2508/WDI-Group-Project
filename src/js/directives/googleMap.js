3d2cc7c6a98afc6ef2db4abe580c80990a806a77
MrBojangles <robbbert.thommmas@gmail.com>
2018-02-03 16:43:33 +0000

    maps stuff

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -4,7 +4,8 @@ angular
 
 googleMap.$inject = ['$window'];
 function googleMap($window) {
-  const markers = [];
+  let firstMarker = null;
+  let secondMarker = null;
   return {
     restrict: 'E',
     replace: true,
@@ -16,29 +17,46 @@ function googleMap($window) {
       const map = new $window.google.maps.Map(element[0], {
         zoom: 4,
         center: scope.center
+
       });
 
       // const marker = new $window.google.maps.Marker({
       //   position: scope.center,
       //   map
       // });
-      function addMarker(e) {
-        const marker = new $window.google.maps.Marker({
-          position: e.latLng,
-          map
-        });
-        console.log(marker);
-        const infoWindow = new $window.google.maps.InfoWindow({
-          content: `${marker.lat} ${marker.lng}`
-        });
-        markers.push(marker);
-        marker.addListener('click', () => {
-          infoWindow.open(map, marker);
-        });
-        return marker;
+      function addFirstMarker(e) {
+        if (firstMarker === null) {
+          firstMarker = new $window.google.maps.Marker({
+            position: e.latLng,
+            map
+          });
+          const infoWindow = new $window.google.maps.InfoWindow({
+            content: `${e.latLng}`
+          });
+          firstMarker.addListener('click', () => {
+            infoWindow.open(map, firstMarker);
+          });
+        } else {
+          firstMarker.position = e.latLng;
+        }
+
+      }
+      function addSecondMarker(e) {
+        if ( secondMarker === null) {
+          secondMarker = new $window.google.maps.Marker({
+            position: e.latLng,
+            map
+          });
+          const infoWindow = new $window.google.maps.InfoWindow({
+            content: `${e.latLng}`
+          });
+          secondMarker.addListener('click', () => {
+            infoWindow.open(map, secondMarker);
+          });
+        }
       }
       map.addListener('click', (e) => {
-        addMarker(e);
+        addFirstMarker(e);
 
       });
     }
