910385ab0cd7dbad513cec233998b19684f3bcb3
Mark S <markscicluna@hotmail.com>
2018-02-03 15:59:09 +0000

    authentication added

---
+++ src/js/factories/errorHandler.js
@@ -0,0 +1,14 @@
+angular
+  .module('roadTrippers')
+  .factory('ErrorHandler', ErrorHandler);
+
+ErrorHandler.$inject = ['$rootScope'];
+
+function ErrorHandler($rootScope) {
+  return {
+    responseError: function(err) {
+      $rootScope.$broadcast('error', err);
+      throw err;
+    }
+  };
+}
