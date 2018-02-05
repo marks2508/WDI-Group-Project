edca5836639a30245f06b5f6797c1a020b54f0cc
Jack Durham <jackdurham11@gmail.com>
2018-02-05 10:37:16 +0000

    added image model, broke it... if anyone can figure it out

--- models/trip.js
+++ models/trip.js
@@ -9,7 +9,8 @@ const mongoose = require('mongoose');
 
 const tripSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true }
+  description: { type: String, required: true },
+  image: { type: String, required: true }
   // startPointLat: { type: Number, required: true },
   // startPointLng: { type: Number, required: true },
   // endPointLat: { type: Number, required: true },
