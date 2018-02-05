ae51744ec2f3cb7280056f3e87ae842e4fdc3abe
Mark S <markscicluna@hotmail.com>
2018-02-05 12:04:55 +0000

    geocode now working

--- src/js/controllers/trips/show.controller.js
+++ src/js/controllers/trips/show.controller.js
@@ -2,16 +2,35 @@ angular
   .module('roadTrippers')
   .controller('TripsShowCtrl', TripsShowCtrl);
 
-TripsShowCtrl.$inject = ['Trip', 'TripComment', '$state'];
-function TripsShowCtrl(Trip, TripComment, $state) {
+TripsShowCtrl.$inject = ['Trip', 'TripComment', '$state', '$http', '$rootScope'];
+function TripsShowCtrl(Trip, TripComment, $state, $http, $rootScope) {
   const vm = this;
   // vm.trip = Trip.get($state.params);
+  vm.geocode = geocode;
+
   Trip
     .get($state.params)
     .$promise
     .then((trip) => {
       vm.trip = trip;
     });
+
+
+
+  function geocode() {
+    $http
+      .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${vm.address}&key=AIzaSyDFdykQIbI6dMLCGyov2befnGnaHEtKA5w`,
+        { skipAuthorization: true })
+      .then(res => {
+        if (res.data.results.length) {
+          $rootScope.$broadcast('newAddressFound', { address: res.data.results[0] });
+          vm.error = false;
+        } else {
+          vm.result = null;
+          vm.error = true;
+        }
+      });
+  }
 
   function TripsDelete() {
     vm.post
