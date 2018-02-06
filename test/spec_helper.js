70b39f957fe42407c5514444599355c022d27389
Mark S <markscicluna@hotmail.com>
2018-02-06 09:32:47 +0000

    changes so that it now runs properly

--- test/spec_helper.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = 'test';
-
-const chai			 = require('chai');
-global.should        = chai.should();
-global.expect        = chai.expect;
-
-const supertest      = require('supertest');
-const app            = require('../index');
-global.api           = supertest(app);
