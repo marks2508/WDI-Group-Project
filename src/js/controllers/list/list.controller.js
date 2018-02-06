d90ac1cd073ef4e66187fd9affa675d6e1bc6bd6
Mark S <markscicluna@hotmail.com>
2018-02-06 18:26:19 +0000

    added a list function on show page

--- src/js/controllers/list/list.controller.js
+++ src/js/controllers/list/list.controller.js
@@ -2,7 +2,16 @@ angular
   .module('roadTrippers')
   .controller('ListCtrl', ListCtrl);
 
-ListCtrl.$inject = ['$scope']
+ListCtrl.$inject = ['$scope'];
 function ListCtrl($scope) {
+  $scope.skills = [];
 
+  $scope.addSkill = function() {
+    $scope.skills.push({'title': $scope.newSkill, 'done': false});
+    $scope.newSkill = '';
+  };
+
+  $scope.deleteSkill = function(index) {
+    $scope.skills.splice(index);
+  };
 }
