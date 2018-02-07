66bbc2d892e998c83d3374f16d90c902ab659d7d
MrBojangles <robbbert.thommmas@gmail.com>
2018-02-07 12:06:57 +0000

    albums almost done

--- models/trip.js
+++ models/trip.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
 
-// const waypointSchema = new mongoose.Schema({
-//   lat: Number,
-//   lng: Number,
-//   description: String
-// });
-
-// waypointSchema.set('toJSON', { virtuals: true });
-
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
   createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
